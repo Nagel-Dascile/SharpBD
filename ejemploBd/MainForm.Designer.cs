4ab402bd4f08825dbd421fb611291070b85b899d
José Cadenas <jdcadenas@gmail.com>
2026-05-03 16:37:03 -0400

    Conexión realizada

---
+++ ejemploBd/MainForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: jdcad
+ * Date: 3/5/2026
+ * Time: 2:53 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace ejemploBd
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dgvUsuarios = new System.Windows.Forms.DataGridView();
+			this.dataView1 = new System.Data.DataView();
+			((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dgvUsuarios
+			// 
+			this.dgvUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvUsuarios.Location = new System.Drawing.Point(12, 12);
+			this.dgvUsuarios.Name = "dgvUsuarios";
+			this.dgvUsuarios.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+			this.dgvUsuarios.Size = new System.Drawing.Size(658, 274);
+			this.dgvUsuarios.TabIndex = 0;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(691, 371);
+			this.Controls.Add(this.dgvUsuarios);
+			this.Name = "MainForm";
+			this.Text = "Ejemplo Conección Base de datos";
+			((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataView1)).EndInit();
+			this.ResumeLayout(false);
+		}
+		private System.Data.DataView dataView1;
+		private System.Windows.Forms.DataGridView dgvUsuarios;
+	}
+}
---
+++ ejemploBd/MainForm.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: jdcad
+ * Date: 3/5/2026
+ * Time: 2:53 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ejemploBd
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+		
+	{
+		//crea una cadena de conexión
+		private string cadenaConexion =  "Server=localhost;Database=peducativa;Uid=root;Pwd=;";
+		
+			
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			CargarUsuarios();
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		public void CargarUsuarios(){
+		
+			try {
+				using (MySqlConnection conexion = new MySqlConnection(cadenaConexion)) {
+					conexion.Open();
+					string consulta = "SELECT * from usuario";
+					
+					MySqlDataAdapter adaptador = new MySqlDataAdapter(consulta, conexion);
+					DataTable tabla = new DataTable();
+					adaptador.Fill(tabla);
+					dgvUsuarios.DataSource = tabla;
+					
+				}
+			} catch (Exception ex) {
+				
+				MessageBox.Show(string.Format("No se pudo realizar conexion por : {0}",ex.Message));
+			}
+		}
+	}
+}
